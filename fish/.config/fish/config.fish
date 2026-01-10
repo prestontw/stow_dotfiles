22a01b0f9992eedc362b43833709faa8c9071d6b
Preston TW <preston_tunnell_wilson@brown.edu>
2026-01-10 00:49:38 -0500

    Add fish configuration
    Based off of personal annoyances, staship init docs, and /usr/share/bazzite-cli/bling.fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+# ls aliases
+if [ "$(command -v eza)" ]
+    alias ls='eza'
+end
+
+export EDITOR="hx"
+
+if status is-interactive
+    starship init fish | source
+    [ "$(command -v atuin)" ] && eval "$(atuin init fish)"
+end
